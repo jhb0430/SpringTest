02aaf0f9d409d53e0be7b23065aed708b69b62e4
jhb0430 <leky2224@gmail.com>
2024-10-31 17:52:33 +0900

    [배지현] - Spring Test 프로젝트 최초 생성

---
+++ src/test/java/com/jhb0430/spring/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhb0430.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
