dd92b74048bf75347cadbe3c685515cb748dd520
jhb0430 <leky2224@gmail.com>
2024-11-11 18:45:19 +0900

    [배지현] - thymeleaf test02 반복문

--- src/main/java/com/jhb0430/spring/test/thymeleaf/ThymeleafController.java
+++ src/main/java/com/jhb0430/spring/test/thymeleaf/ThymeleafController.java
@@ -1,15 +1,96 @@
 package com.jhb0430.spring.test.thymeleaf;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/thymeleaf")
 @Controller
 public class ThymeleafController {
 
-	@GetMapping("/thymeleaf/test01")
+	@GetMapping("/test01")
 	public String test01() {
 		return "thymeleaf/test01";
 	}
 	
+	@GetMapping("/test02")
+	public String test02(Model model) {
+		
+		
+	 List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("musicRanking",musicRanking);
+/*
+해당 리스트를 컨트롤러에서 Model을 통해서 jsp 에 전달하고, 아래와 같은 화면을 구성하세요.
+
+2. 반복문과 조건
+아래와 같이 멤버십 저장되어 있는 리스트 맵이 아래와 있다.
+멤버십 정보에는 이름, 전화번호, 등급, 포인트 가 저장되어 있다.
+해당 리스트 맵을 컨트롤러에서 Model을 통해서 jsp 에 전달하고, 아래와 같은 화면을 구성하세요.
+		 */
+		
+		List<Map<String, Object>> membership = new ArrayList<>();
+		
+		Map<String, Object> member = new HashMap<>();
+		member.put("name", "손오공");
+		member.put("phoneNumber", "010-1234-5678");
+		member.put("grade", "VIP");
+		member.put("point", 12040);
+		membership.add(member);
+		
+		member = new HashMap<>();
+		member.put("name", "저팔계");
+		member.put("phoneNumber", "010-4321-1234");
+		member.put("grade", "BASIC");
+		member.put("point", 3420);
+		membership.add(member);
+		
+		member = new HashMap<>();
+		member.put("name", "사오정");
+		member.put("phoneNumber", "010-8888-1111");
+		member.put("grade", "GOLD");
+		member.put("point", 1530);
+		membership.add(member);
+		
+		member = new HashMap<>();
+		member.put("name", "삼장");
+		member.put("phoneNumber", "010-0000-1234");
+		member.put("grade", "GOLD");
+		member.put("point", 8450);
+		membership.add(member);
+		
+		member = new HashMap<>();
+		member.put("name", "우마왕");
+		member.put("phoneNumber", "010-1111-2222");
+		member.put("grade", "BASIC");
+		member.put("point", 420);
+		membership.add(member);
+		
+		
+		model.addAttribute("membership", membership);
+		
+		
+		
+		return "thymeleaf/test02";
+	}
+	
+	
+	
+	@GetMapping("/test03")
+	public String test03() {
+		
+		return "thymeleaf/test03";
+	}
 	
 }
